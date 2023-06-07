6ba2d90d9449b8582a191899cf640e6dd4086d75
Max <maxkryvytskyiwork@gmail.com>
2023-06-07 23:29:18 +0300

    fixs

--- main.py
+++ main.py
@@ -3,23 +3,49 @@ flag_exit = True
 # Список контактів.
 contacts = {"Name": "+380935555555", 'Bill': '+380931234567', 'Din': '+380931234588'}
 
+# Обробка помилок.
+def input_error(func):
+    def inner(*argsi,**kwargs): 
+        try:
+            return func(*argsi,**kwargs)
+        except TypeError:
+            print("Wrong command")
+            return main()
+        except IndexError:
+            print('Enter name and phone separated by a space!')
+            return main()
+        except ValueError:
+            print("ValueError") # що повинно статися щоб побачити тебе?
+            return main()
+        except KeyError:
+            print("Enter another name.")
+            return main()
+        except AttributeError:
+            print('Enter command.')
+            return main()
+    return inner
+
 # Асистент вітається у відповідь.
+@input_error
 def hello(_):
     return "How can I help you?"
 
 # Асистент додає дані до книги контактів.
+@input_error
 def add(uzer_input):
     text = uzer_input.split()
     contacts[text[1].capitalize()] = text[2]
     return f"Контакт {text[1].capitalize()} з номером {text[2]} створений"
 
 # Асистент змінює дані в книзі контактів.
+@input_error
 def change(uzer_input):
     text = uzer_input.split()
     contacts[text[1].capitalize()] = text[2]
     return f"Номер телефона {text[1].capitalize()} змінений на : {text[2]}"
     
 # Ассистент за ім'ям знаходить в контактах номер.
+@input_error
 def phone(uzer_input):
     text = uzer_input.split()
     text = [i.capitalize() for i in text]
@@ -27,6 +53,7 @@ def phone(uzer_input):
     return f"Номер телефону {text[1]} це : {contacts[text[1]]}"
 
 # Ассистент показує всі контактні дані.
+@input_error
 def show_all(_):
     result = ""
     for name, num  in contacts.items():
@@ -34,6 +61,7 @@ def show_all(_):
     return result
 
 # Зупиняє роботу асистента.
+@input_error
 def exit_uzer(_):
     global flag_exit
     flag_exit = False
@@ -51,33 +79,12 @@ COMMANDS = {"hello": hello,
             }
 
 # Знаходить команду.
+@input_error
 def handler(uzer_input):
     text = uzer_input.lower()
     for keys in COMMANDS.keys():
         if keys in text:
             return COMMANDS[keys]
-        
-# Обробка помилок.
-def input_error(func):
-    def inner():
-        try:
-            func()
-        except TypeError:
-            print("Такої команди не існує")
-            return main()
-        except IndexError:
-            print('Enter name and phone separated by a space!')
-            return main()
-        except ValueError:
-            print("ValueError") # що повинно статися щоб побачити тебе?
-            return main()
-        except KeyError:
-            print("Enter another name.")
-            return main()
-        except AttributeError:
-            print('Enter command.')
-            return main()
-    return inner
 
 @input_error
 def main():
